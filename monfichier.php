d07467295be8a4b51ecea412e01da17489eb0248
Nans-D <ndelaubert@gmail.com>
2023-12-20 15:23:20 +0100

    ajout explications exercices commentaires

--- monfichier.php
+++ monfichier.php
@@ -8,6 +8,9 @@
 </head>
 
 <body>
+
+    // Conditions : Écrivez un script qui détermine si un nombre est positif, négatif ou zéro.
+
     <?php
     $age = 0;
     if ($age < 0) {
@@ -18,6 +21,9 @@
         echo "l'age est égale à 0";
     }
     ?>
+
+    // Boucles : Créez un script qui affiche les 10 premiers nombres pairs.
+
     <div>
         <?php
         for ($i = 0; $i < 10; $i++) {
@@ -27,6 +33,9 @@
         }
         ?>
     </div>
+
+    // Gestion des Erreurs : Écrivez un script qui tente de diviser deux nombres, mais gère correctement le cas où le diviseur est zéro.
+
     <div>
         <?php
         $number = 10;
@@ -42,6 +51,9 @@
         }
         ?>
     </div>
+
+    // 1. Conditions Avancées
+    // Exercice : Écrivez un script PHP qui détermine si un nombre est un multiple de 3, de 5, des deux (comme dans le jeu FizzBuzz), ou d'aucun. Par exemple, si le nombre est 15, il devrait afficher "FizzBuzz", si c'est 9, il devrait afficher "Fizz", si c'est 10, "Buzz", et si c'est 7, simplement afficher le nombre.
     <div>
         <?php
 
@@ -57,6 +69,9 @@
         }
         ?>
     </div>
+
+    // 2. Boucles et Tableaux
+    // Exercice : Créez un tableau en PHP contenant des noms d'animaux. Utilisez une boucle foreach pour parcourir le tableau et affichez chaque nom d'animal avec un numéro devant (comme une liste numérotée).
     <div>
         <?php
         $array = ["lion", "serpent", "oiseau"];
@@ -65,6 +80,9 @@
         }
         ?>
     </div>
+
+    //3. Gestion des Erreurs avec Fonctions Personnalisées
+    //Exercice : Écrivez une fonction PHP qui divise deux nombres et utilise la gestion des erreurs pour traiter les cas spéciaux. La fonction doit gérer le cas où le diviseur est zéro et également le cas où l'un des arguments n'est pas un nombre. Utilisez des exceptions pour gérer ces erreurs.
     <div>
         <?php
         function test($number, $diviseur)
@@ -84,6 +102,9 @@
 
         ?>
     </div>
+
+    //4. Conditions et Boucles Combinées
+    //Exercice : Utilisez une boucle while ou for pour afficher les 20 premiers nombres, mais pour chaque nombre qui est un multiple de 4, affichez "Multiple de 4" au lieu du nombre.
     <div>
         <?php
         for ($i = 1; $i < 20; $i++) {
@@ -94,6 +115,10 @@
 
         ?>
     </div>
+
+    //5. Fonctions et Gestion des Erreurs
+    //Exercice : Écrivez une fonction PHP qui prend un tableau de nombres comme argument et renvoie la somme des nombres. La fonction doit gérer le cas où l'argument n'est pas un tableau ou si le tableau contient des éléments non numériques, en lançant une exception dans ces cas.
+
     <div>
         <?php
         function isTableau($tableau)
